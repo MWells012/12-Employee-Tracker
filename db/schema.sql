ed5cc047956820aea15fbe4f639d1a619b26c9f1
Michaela Wells <mgwells012@gmail.com>
2023-01-07 14:25:10 -0500

    added more dependencies

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,24 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
+
 USE employee_db; 
 
 CREATE TABLE department(
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    department_id INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE employee(
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY (id)
+    manager_id INT UNSIGNED
 );
