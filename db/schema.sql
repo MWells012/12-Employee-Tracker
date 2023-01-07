33c1bb568296b11fa370ccfa53486d026c58de9c
Michaela Wells <mgwells012@gmail.com>
2023-01-07 13:45:26 -0500

    updated a few things in the schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -10,10 +10,9 @@ CREATE TABLE department(
 CREATE TABLE role(
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,3) NOT NULL,
-    department_id INT NOT NULL
-    INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee(
@@ -21,9 +20,6 @@ CREATE TABLE employee(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    manager_id INT
-    INDEX man_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    manager_id INT,
+    PRIMARY KEY (id)
 );
